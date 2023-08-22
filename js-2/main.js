5e3efdc809a24d31d6ea59eac4d0a9c3f3e12134
st-thangtran2 <thang.tran2@supremetech.vn>
2023-08-22 12:00:58 +0700

    edit stye css2

--- js-2/main.js
+++ js-2/main.js
@@ -1,1 +1,13 @@
-const arr1 = 
+const arr1 = [
+  { id: '1', quantity: 2 },
+  { id: '2', quantity: 2 },
+  { id: '3', quantity: 3 },
+  { id: '1', quantity: 3 },
+];
+
+const arr2 = [
+  { id: '1', quantity: 2 },
+  { id: '3', quantity: 2 },
+];
+
+const arr = arr1.concat(arr2);
